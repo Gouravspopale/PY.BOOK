2497016885740c63476169fe012957e6851ce171
Gouravspopale <gouravspopale111@gmail.com>
2023-05-15 17:15:00 +0530

    (TK).15/5/23

--- A(TK).py
+++ A(TK).py
@@ -1,10 +1,23 @@
 import tkinter as tk
 
+# def call():
+#     input_label=(txt1.get())
+#     print(input_label)
+#     input_labe2=(txt2.get())
+#     print(input_labe2)
+#     input_labe3=(txt3.get())
+#     print(input_labe3)
+#     input_labe4=(txt4.get())
+#     print(input_labe4)
+#     input_labe5=(txt5.get())
+#     print(input_labe5)
+
+
 main = tk.Tk(className="  #COSTUMER DETAILES#")
 main.geometry("600x600")
 main.configure(bg='white')
 
-lable1=tk.Label(main,text=" COSTUMER NAME:").grid(row=0,column=0)
+lable1=tk.Label(main,text="COSTUMER NAME:").grid(row=0,column=0)
 txt1=tk.Entry(main).grid(row=0,column=1)
 
 lable2=tk.Label(main,text="PHONE NUMBER:").grid(row=1,column=0)
@@ -18,6 +31,8 @@ txt4=tk.Entry(main).grid(row=3,column=1)
 
 lable5=tk.Label(main,text="TOTAL AMOUNT INCLUDE ALL TAX:").grid(row=4,column=0)
 txt5=tk.Entry(main).grid(row=4,column=1)
+
+btn6=tk.Button(main,text="submit").grid(row=6,column=1)
 
 main.mainloop()
 
