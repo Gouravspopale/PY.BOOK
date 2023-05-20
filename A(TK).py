4731e726210ce66b13bc4101520bcd552b37261e
Gouravspopale <gouravspopale111@gmail.com>
2023-05-20 17:25:32 +0530

    tk

--- A(TK).py
+++ A(TK).py
@@ -1,38 +1,43 @@
 import tkinter as tk
+import csv 
 
-# def call():
-#     input_labe1=(txt1.get))
-#     print(input_label)
-#     input_labe2=(txt2.get())
-#     print(input_labe2)
-#     input_labe3=(txt3.get())
-#     print(input_labe3)
-#     input_labe4=(txt4.get())
-#     print(input_labe4)
-#     input_label5=(txt5.get())
-#     print(input_label5)
+def submit():
+    cust=txt1.get()
+    phonenum=txt2.get()
+    proname=txt3.get()
+    proamt=txt4.get()
+    total=txt1.get()
 
+    with open("C:/Users/User7/Desktop/PY.BOOK/cust.csv","w") as file:
+        data=csv.writer(file)
+        
+        data.writerow([cust,phonenum,proname,proamt,total])
 
 main = tk.Tk(className="  #COSTUMER DETAILES#")
 main.geometry("600x600")
 main.configure(bg='white')
 
 lable1=tk.Label(main,text="COSTUMER NAME:").grid(row=0,column=0)
-txt1=tk.Entry(main).grid(row=0,column=1)
+txt1=tk.Entry(main)
+txt1.grid(row=0,column=1)
 
 lable2=tk.Label(main,text="PHONE NUMBER:").grid(row=1,column=0)
-txt2=tk.Entry(main).grid(row=1,column=1)
+txt2=tk.Entry(main)
+txt2.grid(row=1,column=1)
 
 lable3=tk.Label(main,text="PRODUCT NAME:").grid(row=2,column=0)
-txt3=tk.Entry(main).grid(row=2,column=1)
+txt3=tk.Entry(main)
+txt3.grid(row=2,column=1)
 
 lable4=tk.Label(main,text="PRODUCT AMOUNT:").grid(row=3,column=0)
-txt4=tk.Entry(main).grid(row=3,column=1)
+txt4=tk.Entry(main)
+txt4.grid(row=3,column=1)
 
 lable5=tk.Label(main,text="TOTAL AMOUNT INCLUDE ALL TAX:").grid(row=4,column=0)
-txt5=tk.Entry(main).grid(row=4,column=1)
+txt5=tk.Entry(main)
+txt5.grid(row=4,column=1)
 
-btn6=tk.Button(main,text="submit").grid(row=6,column=1)
+btn6=tk.Button(main,text="submit",command=submit).grid(row=6,column=1)
 
 main.mainloop()
 
